bf48fd5d61e1ed32d4ed30dc82b058685284e9c8
pmoranga <pmoranga@gmail.com>
2016-11-29 13:54:02 +0000

    test

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+class test(
+  $var1 = undef,
+) {
+
+  if $var1 == undef {
+    notice ('var1 is undef')
+  }else{
+    notice("var1 is ${::var1}")
+  }
+
+
+  if $::fact1 == undef {
+    notice ('fact1 is undef when comparing as: $::fact1 == undef')
+  } else {
+    notice('fact1 is not undef when comparing as: $::fact1 == undef')
+  }
+
+  if getvar('::fact1') == undef {
+    notice ('fact1 is undef when comparing as: getvar("::fact1") == undef')
+  } else {
+    notice('fact1 is not undef when comparing as: getvar("::fact1") == undef')
+  }
+
+  if $facts['fact1'] == undef {
+    notice ('fact1 is undef when comparing as: $facts['fact1'] == undef')
+  } else {
+    notice('fact1 is not undef when comparing as: $facts['fact1'] == undef')
+  }
+
+}
---
+++ spec/classes/test_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'filebeat', :type => :class do
+  context 'defaults' do
+    context 'When fact is desired undef ' do
+
+      describe 'using :undef' do
+        let :facts do
+          {
+            :fact1 => :undef
+          }
+        end
+        it { is_expected.to contain_class('test') }
+      end
+      describe 'using nil' do
+        let :facts do
+          {
+            :fact1 => nil
+          }
+        end
+        it { is_expected.to contain_class('test') }
+      end
+
+    end
+  end
+
+end
